43ab057dd1d89d643340aeff32a34ec0d58f8200
jeanziin <jjeanfreitass@gmail.com>
2023-03-02 16:51:07 -0300

    Arquitetura do Software

---
+++ routes/index.php
@@ -0,0 +1,16 @@
+<?php
+
+use function src\slimConfiguration;
+use App\Controllers\ProductController;
+
+
+$app = new \Slim\App(slimConfiguration());
+
+// ===================================
+
+$app->get('/', ProductController::class . ":getProducts");
+
+
+// =====================================
+
+$app->run();
